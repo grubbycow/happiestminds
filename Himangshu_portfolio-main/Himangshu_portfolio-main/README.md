f98f4351a58486b05855cf8ed43112c60c072eb8
Himangshu <himangshu0das@gmail.com>
2024-07-26 11:30:38 +0530

    demo

---
+++ Himangshu_portfolio-main/Himangshu_portfolio-main/README.md
@@ -0,0 +1,2 @@
+# Himangshu_portfolio
+This is a static yet fully responsive website for my portfolio
---
+++ Himangshu_portfolio-main/Himangshu_portfolio-main/portfolio/javascript/script.js
@@ -0,0 +1,8 @@
+//typing animation
+
+var typed = new Typed(".typing", {
+  strings: ["", "CS Graduate", "Web designer", "Web developer", "Graphic Designer"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true
+})
